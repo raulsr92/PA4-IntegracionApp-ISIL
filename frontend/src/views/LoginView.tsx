024465d2c68e42485b147e11500787932a6e7f30
Raul Sanchez R. <raul.sr92@gmail.com>
2025-06-30 14:04:49 -0500

    Avance 1-Hasta dibujo de formulario de registro

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -3,24 +3,16 @@ import {Link} from "react-router-dom"
 export default function LoginView() {
     return(
         <>
-            <div>
-                <img className="w-60" src="/logo.svg" alt="logo" />
-            </div>
-            <div className="text-6xl bg-sky-300">
-                LoginView
-                <h5>De Raul Sanchez</h5>
-            </div>
-            <nav>
-                ¿No tienes una cuenta?
-                <Link className="text-rose-300 hover:text-violet-600 dark:text-white" to="/auth/register">
+            <h1 className="text-center text-4xl text-white font-bold">Iniciar Sesión</h1>
+            <nav className="mt-10 flex gap-x-4 justify-center ">
+                <span className="text-lg text-white ">
+                    ¿No tienes una cuenta?
+                </span>
+                <Link className="text-center text-lg block text-rose-300 hover:text-green-300 dark:text-white" to="/auth/register">
                     Crea una cuenta aquí
                 </Link>
             </nav>
 
         </>
-        
-
-        
-        
     )
 }
--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -2,19 +2,73 @@ import {Link} from "react-router-dom"
 
 export default function RegisterView() {
     return(
-
         <>
-            <div className="text-6xl ">
-                RegisterView
-                <h3 className="text-gray-600">De Raul Sanchez</h3>
-            </div>
-            <nav>
-                ¿Ya tienes una cuenta?
-                <Link className="text-gray-600" to="/auth/login">
+            <h1 className="text-center text-4xl text-white font-bold">Crear cuenta</h1>
+
+            <form 
+                onSubmit={()=>{}}
+                className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10 max-w-md mx-auto">
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="name" className="text-2xl text-slate-500">Nombre</label>
+                    <input 
+                        id="name"
+                        type="text"
+                        placeholder="Nombre"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                    />
+                </div>   
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
+                    <input 
+                        id="email"
+                        type="email"
+                        placeholder="Email de Registro"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                    />
+                </div>    
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="email" className="text-2xl text-slate-500">Handle</label>
+                    <input 
+                        id="handle"
+                        type="text"
+                        placeholder="Nombre de usuario: sin espacios"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                    />
+                </div>      
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="email" className="text-2xl text-slate-500">Password</label>
+                    <input 
+                        id="password"
+                        type="password"
+                        placeholder="Password de Registro"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                    />
+                </div>
+                    <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="email" className="text-2xl text-slate-500">Repetir Password</label>
+                    <input 
+                        id="password"
+                        type="password"
+                        placeholder="Repetir Password"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                    />
+                </div>   
+
+                <input 
+                    type="submit"
+                    className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold" 
+                    value="Crear Cuenta"
+                />                                 
+            </form>
+
+            <nav className="mt-10 flex gap-x-4 justify-center ">
+                <span className="text-lg text-white ">
+                    ¿Ya tienes una cuenta?
+                </span>
+                <Link className="text-center text-lg block text-rose-300 hover:text-green-300 dark:text-white" to="/auth/login">
                     Inicia sesión
                 </Link>
             </nav>
-
         </>
         
     )
